4de3a633becee505654fa7a89516ef7196c69e63
Kim Minh Thang <thangved02@gmail.com>
2022-11-15 17:29:33 +0700

    Optimize

--- webpack.config.js
+++ webpack.config.js
@@ -11,63 +11,71 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 const path = require("path");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "./docs"),
-    publicPath: "",
-  },
+module.exports = (env, argv) => {
+  const isProduct = argv.mode === "production";
 
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "./docs"),
-    },
-    compress: true,
-    port: 9000,
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(mp3|wav)$/i,
-        loader: "file-loader",
-        options: {
-          name: "[name].[contenthash].[ext]",
-        },
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: "single",
-    minimizer: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-    splitChunks: {
-      chunks: "all",
-    },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./index.html"),
-      favicon: path.resolve(__dirname, "./src/assets/images/Duck.png"),
-      inject: true,
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "./public"),
-          to: "assets",
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin(),
+  const productionPlugins = [
     new HTMLInlineCSSWebpackPlugin(),
     new HtmlInlineScriptPlugin(),
     new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/.+/]),
-  ],
+  ];
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "./docs"),
+      publicPath: "",
+    },
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "./docs"),
+      },
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(mp3|wav)$/i,
+          loader: "file-loader",
+          options: {
+            name: "[name].[contenthash].[ext]",
+          },
+        },
+      ],
+    },
+    optimization: {
+      runtimeChunk: "single",
+      minimizer: true,
+      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "./index.html"),
+        favicon: path.resolve(__dirname, "./src/assets/images/Duck.png"),
+        inject: true,
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "./public"),
+            to: "assets",
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin(),
+      ...(isProduct ? productionPlugins : []),
+    ],
+  };
 };
